aa046f0527cfc306368783208311b6b06e574eb1
vadim-mg <vadikm1981@yandex.ru>
2023-04-25 16:30:47 +0500

    install: add entry points

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
 
 module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.[contenthash].js',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "template.html"),
