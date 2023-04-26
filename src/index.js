0822eac132fec57cec8c76286c58f2794005c794
vadim-mg <vadikm1981@yandex.ru>
2023-04-26 12:19:20 +0500

    feat: add alternative locale

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import "./assets/styles/main.scss"
 import { caption } from "./assets/js/caption"
 import { textarea } from "./assets/js/textarea"
 import { Keyboard } from "./assets/js/keyboard"
-import primaryKeys from "./assets/keys.json"
+import keys from "./assets/keys.json"
+import keysRu from "./assets/keys-ru.json"
 
 const body = document.querySelector("body")
 
 body.append(caption)
 body.append(textarea)
-body.append(new Keyboard(primaryKeys).board)
+const keyboard = new Keyboard(keys, keysRu)
+body.append(keyboard.board())
